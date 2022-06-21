45859c695ee95323aa1aef205f7c72ede9cf2e48
Bestine <bestineokinda@gmail.com>
2022-06-21 10:59:14 +0300

    Added the JFrame introductory codes

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -7,12 +7,12 @@ import javax.swing.JFrame;
 public class MyFrame extends JFrame{
 	
 	MyFrame(){
-//		JFrame frame =  new JFrame("MustafaGUI");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		this.setTitle("Mustafa GUI");
-		this.getContentPane().setBackground(Color.red);
-		this.setSize(420, 420);
+		JFrame frame =  new JFrame();
+		this.setTitle("MustafaGUI");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(true);
 		this.setVisible(true);
+		this.setSize(3840, 2160);
 	}
 
 }
