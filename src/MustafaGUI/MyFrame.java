bca9ae8a62189fc7f0c3b664e81709cbac6e3852
Bestine <bestineokinda@gmail.com>
2022-06-22 11:43:12 +0300

    Started Learning labels-added a basic one

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -3,17 +3,24 @@ package MustafaGUI;
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame{
 	
 	MyFrame(){
+		JLabel label = new JLabel();
+		label.setText("Mustafa, do you even code?");
+		
+		
 		JFrame frame =  new JFrame();
 		this.setTitle("MustafaGUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(true);
 		this.setVisible(true);
 		this.setSize(420, 420);
-		this.getContentPane().setBackground(Color.BLUE);
+		
+		// add the components 
+		this.add(label);
 		
 	}
 
