08bc1681ffdedc246982ac06178311ea1d691753
Bestine <bestineokinda@gmail.com>
2022-06-23 14:49:36 +0300

    Updating the changes on JLabel

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -2,14 +2,18 @@ package MustafaGUI;
 
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class MyFrame extends JFrame{
 	
 	MyFrame(){
+		ImageIcon image = new ImageIcon("images/street_analyst.jpeg");
+		
 		JLabel label = new JLabel();
 		label.setText("Mustafa, do you even code?");
+		label.setIcon(image);
 		
 		
 		JFrame frame =  new JFrame();
