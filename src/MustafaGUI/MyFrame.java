d0022f43a32863db9e493e4a77c40d8ab9809f6b
Bestine <bestineokinda@gmail.com>
2022-06-24 19:31:47 +0300

    Added a border to the created label

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -1,23 +1,32 @@
 package MustafaGUI;
 
 import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 public class MyFrame extends JFrame{
 	
 	MyFrame(){
 		ImageIcon image = new ImageIcon("images/street_analyst.jpeg");
-		
+		Border border = BorderFactory.createLineBorder(Color.green, 3);
 		
 		JLabel label = new JLabel();
 		label.setText("Mustafa, the street_analyst");
 		label.setIcon(image);
 		label.setBounds(0, 0, 80, 80);
-		label.setHorizontalTextPosition(JLabel.RIGHT);
+		label.setHorizontalTextPosition(JLabel.CENTER);
 		label.setVerticalTextPosition(JLabel.TOP);
+		label.setForeground(Color.GREEN);
+		label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+		label.setBackground(Color.BLACK);
+		label.setOpaque(true);
+		label.setIconTextGap(0);
+		label.setBorder(border);
 		
 		
 		JFrame frame =  new JFrame();
