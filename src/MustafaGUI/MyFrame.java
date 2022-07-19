2d2acf5a215ba25906468359ca16add6c702b8e2
Bestine <bestineokinda@gmail.com>
2022-07-19 11:43:05 +0300

    Started the JLayered Panes

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -1,27 +1,41 @@
 package MustafaGUI;
 
 
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 public class MyFrame extends JFrame{
 
 	MyFrame(){
-		this.add(new JButton("1"));
-		this.add(new JButton("2"));
-		this.add(new JButton("3"));
-		this.add(new JButton("4"));
-		this.add(new JButton("5"));
-		this.add(new JButton("6"));
-		this.add(new JButton("7"));
-		this.add(new JButton("8"));
-		this.add(new JButton("9"));
+		JLabel label1 = new JLabel();
+		label1.setBounds(50, 50, 200, 200);
+		label1.setBackground(Color.RED);
+		label1.setOpaque(true);
 		
-		this.setLayout(new GridLayout(3, 3, 10, 10));
+		JLabel label2 = new JLabel();
+		label2.setBounds(100, 100, 200, 200);
+		label2.setBackground(Color.GREEN);
+		label2.setOpaque(true);
+		
+		JLabel label3 = new JLabel();
+		label3.setBounds(150, 150, 200, 200);
+		label3.setBackground(Color.BLUE);
+		label3.setOpaque(true);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 0, 500, 500);
+		layeredPane.add(label1);
+		layeredPane.add(label2);
+		layeredPane.add(label3);
+		
+		this.setLayout(null);
+		this.add(layeredPane);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(500, 500);
+		this.setSize(new Dimension(500, 500));
 		this.setVisible(true);
 	}
 }
