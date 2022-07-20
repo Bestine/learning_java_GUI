92b9feb2f0dbc540829914919f17e79d4c348d4f
Bestine <bestineokinda@gmail.com>
2022-07-20 10:01:48 +0300

    Done with JTextField

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -2,24 +2,42 @@ package MustafaGUI;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame implements ActionListener{
 	
 	JTextField textField;
+	JButton button;
 
 	MyFrame(){
-		textField = new JTextField();
-		textField.setPreferredSize(new Dimension(150, 30));
+		button = new JButton("submit");
+		button.setFocusable(false);
+		button.addActionListener(this);
 		
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(150, 30));		
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
 		
 		this.add(textField);
+		this.add(button);
+		
 		this.setVisible(true);
 		this.setSize(300, 300);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			String text = textField.getText();
+			System.out.println("Hello, " + text);
+		}
+		
 	}
 }
