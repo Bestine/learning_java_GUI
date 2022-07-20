fc59935d89eeb633ab4f51c0c65289ab238dfcb8
Bestine <bestineokinda@gmail.com>
2022-07-20 09:35:37 +0300

    Done with Option panes

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -1,41 +1,25 @@
 package MustafaGUI;
 
-
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
+import javax.swing.JTextField;
 
 public class MyFrame extends JFrame{
+	
+	JTextField textField;
 
 	MyFrame(){
-		JLabel label1 = new JLabel();
-		label1.setBounds(50, 50, 200, 200);
-		label1.setBackground(Color.RED);
-		label1.setOpaque(true);
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(150, 30));
 		
-		JLabel label2 = new JLabel();
-		label2.setBounds(100, 100, 200, 200);
-		label2.setBackground(Color.GREEN);
-		label2.setOpaque(true);
 		
-		JLabel label3 = new JLabel();
-		label3.setBounds(150, 150, 200, 200);
-		label3.setBackground(Color.BLUE);
-		label3.setOpaque(true);
-		
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(0, 0, 500, 500);
-		layeredPane.add(label1, Integer.valueOf(2));
-		layeredPane.add(label2, Integer.valueOf(1));
-		layeredPane.add(label3, Integer.valueOf(0));
-		
-		this.setLayout(null);
-		this.add(layeredPane);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(new Dimension(500, 500));
+		this.setLayout(new FlowLayout());
+		
+		this.add(textField);
 		this.setVisible(true);
+		this.setSize(300, 300);
 	}
 }
--- src/MustafaGUI/OptionPanes.java
+++ src/MustafaGUI/OptionPanes.java
@@ -8,9 +8,13 @@ public class OptionPanes extends JOptionPane{
 		//this.showMessageDialog(null,"Some Useless info","Just A plain Message",JOptionPane.PLAIN_MESSAGE);
 		//this.showMessageDialog(null,"Some Useless info","Just an Information Message",JOptionPane.INFORMATION_MESSAGE);
 		//this.showMessageDialog(null,"Some Useless Question","Just A Question Message?",JOptionPane.QUESTION_MESSAGE);
-//		this.showMessageDialog(null,"Some Useless Warning","Warning!",JOptionPane.WARNING_MESSAGE);
-		this.showMessageDialog(null,"Some Useless Error","Syntax Error",JOptionPane.ERROR_MESSAGE);
+		//this.showMessageDialog(null,"Some Useless Warning","Warning!",JOptionPane.WARNING_MESSAGE);
+		//this.showMessageDialog(null,"Some Useless Error","Syntax Error",JOptionPane.ERROR_MESSAGE);
 		
+		//this.showConfirmDialog(null, "Mustafa, do you even code?", "title", JOptionPane.YES_NO_CANCEL_OPTION);
+		String name = this.showInputDialog(null, "What is your name?");
+		
+		System.out.println("Hello, " + name);
 	}
 
 }
