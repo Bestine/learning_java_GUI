5cdced4d96199bae8e12187beb100a9d6c4f5f0a
Bestine <bestineokinda@gmail.com>
2022-07-28 10:22:47 +0300

    Started on JCheckboxes

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -1,43 +1,22 @@
 package MustafaGUI;
 
-import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 
-public class MyFrame extends JFrame implements ActionListener{
-	
-	JTextField textField;
-	JButton button;
+public class MyFrame extends JFrame{
 
 	MyFrame(){
-		button = new JButton("submit");
-		button.setFocusable(false);
-		button.addActionListener(this);
 		
-		textField = new JTextField();
-		textField.setPreferredSize(new Dimension(150, 30));		
+		JCheckBox checkBox = new JCheckBox();
+		checkBox.setText("Are you Mustafa?");
 		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		this.add(textField);
-		this.add(button);
-		
+		this.add(checkBox);
+		this.pack();
 		this.setVisible(true);
-		this.setSize(300, 300);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==button) {
-			String text = textField.getText();
-			System.out.println("Hello, " + text);
-		}
-		
 	}
 }
