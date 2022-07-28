7a09f82a4eab0e17fe752777e11295e764d4c924
Bestine <bestineokinda@gmail.com>
2022-07-28 15:02:01 +0300

    Done with JComboboxes

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -4,40 +4,25 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JRadioButton;
 
 public class MyFrame extends JFrame implements ActionListener{	
 	
-	JRadioButton vegButton;
-	JRadioButton fruitButton;
-	JRadioButton saladButton;
+	JComboBox comboBox;
 	
 	MyFrame(){
-		vegButton = new JRadioButton();
-		vegButton.setText("vegetable");
-		vegButton.addActionListener(this);
-		
-		fruitButton = new JRadioButton();
-		fruitButton.setText("fruit");
-		fruitButton.addActionListener(this);
-		
-		saladButton = new JRadioButton();
-		saladButton.setText("salad");
-		saladButton.addActionListener(this);
-		
-		ButtonGroup buttonGroup = new ButtonGroup();
-		buttonGroup.add(fruitButton);
-		buttonGroup.add(vegButton);
-		buttonGroup.add(saladButton);
+		String[] vegetables = {"spinach", "tomato", "cabbage"};
 
-		this.add(fruitButton);
-		this.add(vegButton);
-		this.add(saladButton);
+		comboBox = new JComboBox(vegetables);
+		comboBox.addActionListener(this);
+//		comboBox.setEditable(true);
+//		comboBox.removeAllItems();
 		
 		this.setLayout(new FlowLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.add(comboBox);
 		
 		this.pack();
 		this.setVisible(true);
@@ -45,15 +30,8 @@ public class MyFrame extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==vegButton) {
-			System.out.println("Ordered vegetables");
+		if(e.getSource()==comboBox) {
+			System.out.println(comboBox.getSelectedItem());
 		}
-		else if(e.getSource()==fruitButton) {
-			System.out.println("Ordered fruits");
-		}
-		else if(e.getSource()==saladButton) {
-			System.out.println("Ordered salad");
-		}
-			
 	}
 }
