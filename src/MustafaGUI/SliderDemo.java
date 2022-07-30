2983d9df5f2a0e7ee22089b526b50d03c2ba781b
Bestine <bestineokinda@gmail.com>
2022-07-30 11:42:31 +0300

    Done with learning JSliders

--- src/MustafaGUI/SliderDemo.java
+++ src/MustafaGUI/SliderDemo.java
@@ -12,15 +12,35 @@ public class SliderDemo implements ChangeListener{
 	JFrame frame;
 	
 	SliderDemo() {
+		slider = new JSlider(0, 100, 40);
+		slider.setPreferredSize(new Dimension(500, 200));
+		slider.addChangeListener(this);
+		slider.setMinorTickSpacing(5);
+		slider.setMajorTickSpacing(10);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		//slider.setOrientation(JSlider.VERTICAL);
+		slider.setOrientation(JSlider.HORIZONTAL);
 		
-		frame.setSize(600, 350);
+		
+		label = new JLabel();
+		label.setText(" " + slider.getValue()+"°C");
+		label.setFont(new Font("MV Boli", Font.BOLD, 25));
+		
+		panel = new JPanel();
+		panel.add(slider);
+		panel.add(label);
+		
+		frame = new JFrame();
+		frame.add(panel);
+		frame.setSize(620, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 	}
 
 	@Override
-	public void stateChanged(ChangeEvent arg0) {
-		// TODO Auto-generated method stub
+	public void stateChanged(ChangeEvent e) {
+		label.setText(" " + slider.getValue()+"°C");;
 		
 	}
 	
