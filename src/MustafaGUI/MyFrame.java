b886bc617686aaa596c91b494be20d103bc556ce
Bestine <bestineokinda@gmail.com>
2022-08-01 10:38:38 +0300

    Done with JMenuBar

--- src/MustafaGUI/MyFrame.java
+++ src/MustafaGUI/MyFrame.java
@@ -4,34 +4,58 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MyFrame extends JFrame implements ActionListener{	
 	
-	JComboBox comboBox;
-	
-	MyFrame(){
-		Integer[] vegetables = {1997, 25, 2015, 2011};
+	JMenuItem loadItem;
+	JMenuItem saveItem;
+	JMenuItem exitItem;
 
-		comboBox = new JComboBox(vegetables);
-		comboBox.addActionListener(this);
-//		comboBox.setEditable(true);
-//		comboBox.removeAllItems();
-		
+	MyFrame(){		
 		this.setLayout(new FlowLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		this.add(comboBox);
+		JMenuBar menuBar = new JMenuBar();
 		
-		this.pack();
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu helpMenu = new JMenu("Help");
+		
+		loadItem  = new JMenuItem("Load");
+		loadItem.addActionListener(this);
+		saveItem  = new JMenuItem("Save");
+		saveItem.addActionListener(this);
+		exitItem  = new JMenuItem("Exit");
+		exitItem.addActionListener(this);
+		
+		fileMenu.add(loadItem);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+		
+		this.setJMenuBar(menuBar);
+		this.setSize(300, 300);
 		this.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==comboBox) {
-			System.out.println(comboBox.getSelectedItem());
+		if(e.getSource()==loadItem) {
+			System.out.println("You just loaded");
 		}
+		if(e.getSource()==saveItem) {
+			System.out.println("You just saved");
+		}
+		if(e.getSource()==exitItem) {
+			System.exit(0);
+		}
+		
 	}
 }
